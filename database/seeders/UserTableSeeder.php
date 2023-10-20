2b10176f0c678d208d86ac5431588e7bbe8c8f48
Retno07 <retnomadani07@gmail.com>
2023-10-20 14:38:56 +0700

    20-10-2023
    membuat seeder user admin

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash; 
 
 class UserTableSeeder extends Seeder
 {
@@ -14,5 +15,11 @@ class UserTableSeeder extends Seeder
     {
         // factory(\App\User::class, 5)->create();
         \App\Models\User::factory()->count(5)->create(); 
+
+        \App\Models\User::create([
+            'name'=>'Admin',
+            'email'=>'admin@gmail.com',
+            'password'=>Hash::make('adminadmin'),
+        ]);
     }
 }
